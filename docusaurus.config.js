a04960d768824a495116a181cffffbf03dd1eeb3
Tehnalogos <none>
2026-06-22 16:25:54 +0300

    address concerns from Fabian and Jean

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -147,12 +147,6 @@ export default {
         },
         {
           type: 'doc',
-          docId: 'evm/overview',
-          position: 'left',
-          label: 'EVM Builders',
-        },
-        {
-          type: 'doc',
           docId: 'networks/mainnet/parameters',
           position: 'left',
           label: 'Networks',
